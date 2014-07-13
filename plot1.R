b04734749e9f1aa043989ce39a079bf69567646e
rherena <ricardoherena@gmail.com>
2014-07-13 15:59:58 -0500

    finished plot 1 code
    plot 1 code should create the plot

--- plot1.R
+++ plot1.R
@@ -1,2 +1,29 @@
 setwd("~/Data/datasciencecoursera/DataAnalysis/Assignment 1")
-library(datasets)
+
+
+if ( !file.exists("data")) {
+        dir.create("data")
+}
+
+
+
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+
+download.file(fileurl, destfile = "./data/Power.zip", method = "curl")
+unzip("./data/Power.zip", exdir = "./data")
+
+
+
+powerdata<- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", nrows = 70000, colClasses = "character")
+
+powerdata[, 1] <- as.Date(powerdata[, 1],format = "%d/%m/%Y")
+
+powerdata[, 3] <- as.numeric(powerdata[, 3])
+
+SelectedData <- subset(powerdata, as.Date(Date) >= '2007-02-01' & as.Date(Date) <= '2007-02-02') 
+
+png(file = "plot1.png")
+hist(SelectedData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
+
--- plot2.R
+++ plot2.R
@@ -1,1 +1,3 @@
 setwd("~/Data/datasciencecoursera/DataAnalysis/Assignment 1")
+powerdata<- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 70000)
+
