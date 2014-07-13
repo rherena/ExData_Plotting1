a02b9da0f65d82342477a89f6e92c28155704428
rherena <ricardoherena@gmail.com>
2014-07-13 17:04:09 -0500

    Completed Plot 2
    Plot 2 Code and chart completed

--- plot1.R
+++ plot1.R
@@ -6,13 +6,12 @@ if ( !file.exists("data")) {
 }
 
 
-
-fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-
-
-download.file(fileurl, destfile = "./data/Power.zip", method = "curl")
-unzip("./data/Power.zip", exdir = "./data")
-
+### if the files have not been downloaded bring them in and unzip
+if ( !file.exists("./data/household_power_consumption.txt")){
+        fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileurl, destfile = "./data/Power.zip", method = "curl")
+        unzip("./data/Power.zip", exdir = "./data")
+}
 
 
 powerdata<- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", nrows = 70000, colClasses = "character")
@@ -23,7 +22,7 @@ powerdata[, 3] <- as.numeric(powerdata[, 3])
 
 SelectedData <- subset(powerdata, as.Date(Date) >= '2007-02-01' & as.Date(Date) <= '2007-02-02') 
 
-png(file = "plot1.png")
+png(file = "plot1.png", width=480,height=480,units='px')
 hist(SelectedData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()
 
--- plot2.R
+++ plot2.R
@@ -1,3 +1,40 @@
-setwd("~/Data/datasciencecoursera/DataAnalysis/Assignment 1")
-powerdata<- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 70000)
+setwd("~/Data/datasciencecoursera/DataAnalysis/Assignment 1/ExData_Plotting1")
+
+
+if ( !file.exists("data")) {
+        dir.create("data")
+}
+
+### if the files have not been downloaded bring them in and unzip
+if ( !file.exists("./data/household_power_consumption.txt")){
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "./data/Power.zip", method = "curl")
+unzip("./data/Power.zip", exdir = "./data")
+}
+
+
+powerdata<- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", nrows = 70000, colClasses = "character")
+
+powerdata[, 1] <- as.Date(powerdata[, 1],format = "%d/%m/%Y")
+
+powerdata[, 3] <- as.numeric(powerdata[, 3])
+
+
+## Subsets the data required
+SelectedData <- subset(powerdata, as.Date(Date) >= '2007-02-01' & as.Date(Date) <= '2007-02-02') 
+
+### Need to alter the data incorporate the time in the date and change the date to thur friday sat. 
+time <- as.character(SelectedData$Time)
+date <- as.character(SelectedData$Date)
+datetime <- paste(date, time)
+datetime <- strptime(datetime, "%Y-%m-%d %H:%M:%S")
+
+
+## Create plots
+png(file = "plot2.png", width=480,height=480,units='px')
+
+### Creates the plot specific that is being saved.
+plot(datetime,SelectedData$Global_active_power, ylab= "Global Active Power (kilowatts)", xlab = "", type= "l")
+
+dev.off()
 
